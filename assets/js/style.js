ac4fb63173b656e189776eeedbcbe93a25cf10e1
Soumyapradhan93 <soumyaphadhan.2004@gmail.com>
2025-04-28 14:46:27 +0530

    Update style.js
    first

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,4 +1,33 @@
 
+// header title
+
+<script>
+const texts = ["Welcome Students!", "Learn JavaScript!", "Build Awesome Projects!"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById('typewriter').textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1000); // Pause before next text
+  } else {
+    setTimeout(type, 100);
+  }
+}
+
+type();
+</script>
 
 // smooth scroll
 $(document).ready(function(){
